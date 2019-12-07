f7bb24fdbcd571d77c1ef63472dabeb399000979
JGS <jan.duchacek.bc@gmail.com>
2019-12-07 11:36:18 +0100

    Created base sql query builder. Created enum with possible entry separators

---
+++ RawSqlHelper/EntrySeparator.cs
@@ -0,0 +1,23 @@
+﻿namespace RawSqlHelper
+{
+    /// <summary>
+    /// Separator for each entry
+    /// </summary>
+    public enum EntrySeparator
+    {
+        /// <summary>
+        /// Only adding text one by one
+        /// </summary>
+        Append,
+
+        /// <summary>
+        /// Each entry is ensured to end with space. Then added one by one 
+        /// </summary>
+        AppendWithSpace,
+
+        /// <summary>
+        /// Each entry is added on new line
+        /// </summary>
+        AppendWithNewLine
+    }
+}
