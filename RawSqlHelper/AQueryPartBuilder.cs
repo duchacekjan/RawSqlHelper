c1bbb68ef12056d172e55267c4a1126253bc2aec
JGS <jan.duchacek.bc@gmail.com>
2019-12-08 17:08:08 +0100

    Created base for part builders

---
+++ RawSqlHelper/AQueryPartBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace RawSqlHelper
+{
+    /// <summary>
+    /// Base for builders, which participates in building raw SQL query
+    /// </summary>
+    public abstract class AQueryPartBuilder
+    {
+        /// <summary>
+        /// Resulted part SQL Query
+        /// </summary>
+        protected abstract string Value { get; set; }
+
+        /// <summary>
+        /// Returns <see cref="Value"/>
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
